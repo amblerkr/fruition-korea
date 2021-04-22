6c904ff0a9644d5355e9424f9b20eadda1b11541
Ambler <ambler.official@gmail.com>
2021-04-23 04:44:53 +0900

    모바일 상단바 수정

--- worker.js
+++ worker.js
@@ -197,8 +197,13 @@ class HeadRewriter {
         }
       );
     }
-    element.append(
-      `<style>
+    element.append(this.newMethod(), {
+      html: true
+    });
+  }
+
+  newMethod() {
+    return `<style>
       div.notion-topbar > div > div:nth-child(4) { display: none !important; }
       div.notion-topbar > div > div:nth-child(5) { display: none !important; }
       div.notion-topbar > div > div:nth-child(6) { display: none !important; }
@@ -208,11 +213,7 @@ class HeadRewriter {
       div.notion-topbar-mobile > div:nth-child(5) { display: none !important; }
       div.notion-topbar > div > div:nth-child(1n).toggle-mode { display: block !important; }
       div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !important; }
-      </style>`,
-      {
-        html: true
-      }
-    );
+      </style>`;
   }
 }
 
